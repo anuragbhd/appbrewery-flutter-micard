4a9271a298526e4d854133ea4383588101ed9989
Anurag Bhandari <anurag.bhd@gmail.com>
2019-08-29 17:47:15 +0530

    Implement Layouts Challenge
    For this challenge, we designed the layout as per the given mock up.

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,32 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            SizedBox(
-              width: 20.0,
-            ),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            SizedBox(
-              width: 20.0,
-            ),
-            Container(
-              height: 100.0,
+              height: double.infinity,
               width: 100.0,
               color: Colors.red,
-              child: Text('Container 3'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: 100.0,
+              color: Colors.blue,
             ),
           ],
         )),
