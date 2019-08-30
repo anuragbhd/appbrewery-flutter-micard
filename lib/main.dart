108b759db80d27aab203afd3f5737ad8ffe96dbb
Anurag Bhandari <anurag.bhd@gmail.com>
2019-08-30 13:36:57 +0530

    Add email section
    This section looks exactly like contact number section but has been
    created from ground up in order to practice a little bit more :)

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,30 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'anurag.bhd@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
